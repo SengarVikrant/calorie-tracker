4edcf92d5e42718aca17c7f8d3d11565af82ad56
VikrantPratap2001 <satyamsingh2110@gmail.com>
2026-05-27 23:02:44 +0530

    init: add Spring Boot project with MySQL configuration

---
+++ src/main/java/com/vikrant/calorietracker/CalorieTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.vikrant.calorietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CalorieTrackerApplication.class, args);
+	}
+
+}
