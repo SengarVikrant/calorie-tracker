ab3c35dc466affc97bef11f8b6a58c2ef79c9e5e
SengarVikrant <sengarvikrant2003@gmail.com>
2026-05-30 14:09:59 +0530

    feat: add Unit entity and UnitType enum

---
+++ src/main/java/com/vikrant/calorietracker/entity/Unit.java
@@ -0,0 +1,17 @@
+package com.vikrant.calorietracker.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Table(name = "unit")
+@Entity
+public class Unit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private UnitType unitType;
+    private Double amountInMl;
+}
