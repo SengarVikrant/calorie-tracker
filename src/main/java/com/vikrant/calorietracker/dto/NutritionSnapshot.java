e5cf37715de08c4c91d0f998be892e6f5e2a711f
SengarVikrant <sengarvikrant2003@gmail.com>
2026-06-13 19:41:41 +0530

    fix: round nutrition values to 2 decimal places

--- src/main/java/com/vikrant/calorietracker/dto/NutritionSnapshot.java
+++ src/main/java/com/vikrant/calorietracker/dto/NutritionSnapshot.java
@@ -1,8 +1,9 @@
 package com.vikrant.calorietracker.dto;
 
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class NutritionSnapshot {
     private double calories;
     private double protein;
@@ -10,4 +11,13 @@ public class NutritionSnapshot {
     private double fat;
     private double fiber;
 
+    private double round(double value) {
+        return Math.round(value * 100.0) / 100.0;
+    }
+
+    public void setCalories(double value) { this.calories = round(value); }
+    public void setProtein(double value)  { this.protein  = round(value); }
+    public void setCarbs(double value)    { this.carbs    = round(value); }
+    public void setFat(double value)      { this.fat      = round(value); }
+    public void setFiber(double value)    { this.fiber    = round(value); }
 }
